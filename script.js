0af0a06e9a8407db9313dcd5908700821c604829
ProstoLama27 <mankaegor25@gmail.com>
2023-04-14 21:39:01 +0500

    burgerMenu is made

---
+++ script.js
@@ -0,0 +1,20 @@
+let counter = 0;
+function dropMenu(){
+    document.getElementById("dropMenu").classList.toggle("dropMenu__wisible");
+    
+}
+// нужно донастроить скрипт: разобраться с подключениемм классов в li,a  и тд
+
+
+// const btn = document.getElementById("btn-play");
+// const circle = document.querySelector(".circle");
+// let titels = ["Пуск", "Стоп"];
+// let current = 0;
+// btn.addEventListener("click",function(){
+//     circle.classList.toggle("play");
+//     current++;
+//     btn.innerText = titels[current];
+//     if(current==1){
+//         current-=2;
+//     }
+// });
